22c7432d1a908c08791fd78c7cc07a0fecde90e1
Bastian Åström <ba222ji@student.lnu.se>
2023-06-28 20:33:33 +0300

    Start, test to commit

---
+++ DiscorBotTime/DiscorBotTime/TimerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscorBotTime
+{
+    public class TimerService
+    {
+
+        public void StartTimer()
+        {
+            var maxTime = 2;
+
+        }
+    }
+}
