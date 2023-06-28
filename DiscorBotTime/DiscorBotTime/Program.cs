24d213b7826955ae96330e63e6e2afa7609cf825
Bastian Åström <ba222ji@student.lnu.se>
2023-06-28 21:40:40 +0300

    Timmer version 1.0!

--- DiscorBotTime/DiscorBotTime/Program.cs
+++ DiscorBotTime/DiscorBotTime/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DiscorBotTime;
+
+Console.WriteLine("Set timer (in minutes)");
+var timeToRecall = Console.ReadLine();
+var msToMin = int.Parse(timeToRecall);
+Console.Clear();
+Console.WriteLine("Timer starts");
+
+msToMin = msToMin * 60;//change name? 
+
+
+var serivice = new TimerService();
+
+serivice.StartTimer(msToMin);
--- DiscorBotTime/DiscorBotTime/TimerService.cs
+++ DiscorBotTime/DiscorBotTime/TimerService.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace DiscorBotTime
 {
     public class TimerService
     {
+        
 
-        public void StartTimer()
+        public void StartTimer(int capTime)
         {
-            var maxTime = 2;
+            //var maxTime = 2;
+            //var timer = new Timer(FinishedTime, null, 0, 100000);
+
+            var count = 1;
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(count);
+                count++;
+
+                if (count > capTime)
+                {
+                    Console.WriteLine("You Suck!");
+
+                    break;
+                }
+            }
+            
+        }
+
+        private static void FinishedTime(object state)
+        {
+            Console.WriteLine("Hej");
+
 
         }
     }
